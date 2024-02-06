dc9b6b5853c103166f2765bb8278189f42709c0d
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2024-02-06 13:22:28 +0900

    Modified app name

---
+++ src/components/Map.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  import "./map.js"
+  const mapEle = '<div id="map"></div>'
+</script>
+
+<template>
+  <link href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" rel="stylesheet" crossorigin="" />
+  <!-- <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script> -->
+    <div id="map"></div>
+</template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 400px;
+  max-width: 600px;
+}
+</style>
---
+++ src/components/map.js
@@ -0,0 +1,49 @@
+const init = () => {
+  const [lat, lon] = [35.6815657, 139.7675949]
+  const zoom = 14
+  const map = L.map('map').setView([lat, lon], zoom)
+
+  const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map)
+  console.log(tiles)
+
+  const marker = L.marker([lat, lon])
+    .addTo(map).bindPopup('<strong>𩸽を𠮟る𠮷野家</strong>').openPopup()
+  console.log(marker)
+
+  const [circleLat, circleLon] = [35.6856189, 139.7525002]
+  const circle = L.circle([circleLat, circleLon], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.2,
+    radius: 1000
+  }).addTo(map).bindPopup(`円の中心は経度${circleLat},緯度${circleLon}`)
+  console.log(circle)
+
+  const polygon = L.polygon([
+    [35.6813576, 139.7784939],
+    [35.6822327, 139.7859957],
+    [35.6697208, 139.7708326],
+    [35.6731245, 139.7583829]
+  ]).addTo(map).bindPopup('サンプルポリゴン')
+  console.log(polygon)
+
+  const popup = L.popup()
+    .setLatLng([35.6885528, 139.7658447])
+    .setContent('サンプルポップアップ').openOn(map);
+  console.log(popup)
+
+  const onMapClick = evt => {
+    console.log(evt)
+    const { lat, lng } = evt.latlng
+    popup.setLatLng(evt.latlng)
+      .setContent(`緯度=${lat}<br />経度=${lng}`)
+      .openOn(map)
+  }
+
+  map.on('click', onMapClick);
+}
+
+init()
